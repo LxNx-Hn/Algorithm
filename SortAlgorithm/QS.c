419fffa19dbdf1444ad68118f91cf6e57eee76a7
Haru <jonggun02@gmail.com>
2024-12-29 17:32:43 +0900

    QS

--- SortAlgorithm/QS.c
+++ SortAlgorithm/QS.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
-
-#define swap(a, b) { int temp = a; a = b; b = temp; }
+#define SWAP(x, y) { int temp = x; x = y; y = temp; }
 
 void OutPut(int Arr[], int len) {
-    for (int i = 0; i < len; i++) 
+    for (int i = 0; i < len; i++) {
         printf("%d ", Arr[i]);
+    }
     printf("\n");
 }
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
+void quickSort(int arr[], int left, int right) {
+    if (left < right) {
 
-    for (int j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+        int pivot = arr[(left + right) / 2]; 
+        int i = left;
+        int j = right;
+
+        while (i <= j) {
+            while (arr[i] < pivot) i++;
+            while (arr[j] > pivot) j--;
+            if (i <= j) {
+                SWAP(arr[i], arr[j]);
+                i++;
+                j--;
+            }
         }
-    }
-    swap(arr[i + 1], arr[high]);
-    return (i + 1);
-}
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        if (left < j) quickSort(arr, left, j);
+        if (i < right) quickSort(arr, i, right);
     }
 }
 
 int main() {
-    int arr[] = {10, 7, 8, 9, 1, 5};
+    int arr[] = {35, 2, 20, 13, 30, 25, 21, 23};
     int len = sizeof(arr) / sizeof(arr[0]);
-    quickSort(arr, 0, len - 1);
+    quickSort(arr, 0, len - 1); 
     OutPut(arr, len);
+
     return 0;
 }
